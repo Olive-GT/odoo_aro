ae34001365874de58721c2478bb2409a62b01a5f
JuancaOlive <juanca@olivegt.com>
2025-03-27 15:48:14 -0600

    changin values position

--- __manifest__.py
+++ __manifest__.py
@@ -1,18 +1,16 @@
 {
-    'name': "Contabilidad Personalizada",
+    'name': "Contabilidad Personalizada (Guatemala - Olive Tech)",
     'summary': "Añade campos personalizados y validaciones a las facturas",
     'description': """
         Agrega campos Serie, Numero, Tipo_DTE y Descripcion_general.
         También verifica que no se repitan Serie y Numero juntos.
     """,
-    'author': "Tu Nombre",
+    'author': "Olive Tech",
     'website': "",
     'category': 'Accounting',
     'version': '18.0.1.0',
     'depends': ['account'],
-    'data': [
-        'views/account_move_views.xml',
-    ],
+    'data': [],
     'installable': True,
     'application': False,
     'license': 'LGPL-3',
