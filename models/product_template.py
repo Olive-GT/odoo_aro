01580645f44a599515230b1e487581547e50f92e
JuancaOlive <juanca@olivegt.com>
2025-03-31 11:47:52 -0600

    adding type to product

---
+++ models/product_template.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    type = fields.Selection(selection_add=[
+        ('combustible', 'Combustible')
+    ])
