2ebff4f2204572a7595e9cd67b2b05cbf1c1f988
JuancaOlive <juanca@olivegt.com>
2025-03-31 10:09:51 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -11,7 +11,7 @@
     'version': '18.0.1.0',
     'depends': ['account', 'base'],
     'data': [
-        'views/res_config_settings_view.xml',
+        #'views/res_config_settings_view.xml',
     ],
     'installable': True,
     'application': False,
