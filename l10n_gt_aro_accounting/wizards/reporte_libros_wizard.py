af4047ab3ca7fe925531e31ec9b61c4cf5a3d6e2
JuancaOlive <juanca@olivegt.com>
2025-03-31 17:45:44 -0600

    Update reporte_libros_wizard.py

--- l10n_gt_aro_accounting/wizards/reporte_libros_wizard.py
+++ l10n_gt_aro_accounting/wizards/reporte_libros_wizard.py
@@ -22,7 +22,24 @@ class ReporteLibrosWizard(models.TransientModel):
         data = self.env['reporte.fiscal.utils'].get_factura_data(
             self.date_start, self.date_end, self.journal_id, self.libro
         )
-        return self.env.ref('l10n_gt_aro_accounting.action_report_libros_pdf').report_action(self, data={'lines': data, 'folio_inicial': self.folio_inicial})
+        
+        # Get the report action by searching instead of using ref
+        report_action = self.env['ir.actions.report'].search([
+            ('report_name', '=', 'l10n_gt_aro_accounting.reporte_libros_pdf_template')
+        ], limit=1)
+        
+        if not report_action:
+            # The report doesn't exist, let's create it
+            report_action = self.env['ir.actions.report'].create({
+                'name': 'Reporte Libros PDF',
+                'model': 'reporte.libros.wizard',
+                'report_type': 'qweb-pdf',
+                'report_name': 'l10n_gt_aro_accounting.reporte_libros_pdf_template',
+                'report_file': 'l10n_gt_aro_accounting.reporte_libros_pdf_template',
+                'print_report_name': "'Reporte_Fiscal_' + object.libro",
+            })
+        
+        return report_action.report_action(self, data={'lines': data, 'folio_inicial': self.folio_inicial})
 
     def action_generar_excel(self):
         return self.env.ref('l10n_gt_aro_accounting.reporte_libros_excel').report_action(self, data={
