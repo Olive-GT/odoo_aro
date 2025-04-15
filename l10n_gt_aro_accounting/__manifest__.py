21d854a67ebd2d18228c9cefd7c832a97a7ce279
JuancaOlive <juanca@olivegt.com>
2025-04-14 19:03:54 -0600

    Update __manifest__.py

--- l10n_gt_aro_accounting/__manifest__.py
+++ l10n_gt_aro_accounting/__manifest__.py
@@ -17,10 +17,10 @@
     'data': [
         'security/ir.model.access.csv',
         'views/res_config_settings_view.xml',
+        'data/report_paperformat.xml',
         'views/reporte_libros_views.xml',
         'views/reporte_libros_templates.xml',
         'reports/libros_report_action.xml',
-        'data/report_paperformat.xml'
     ],
     'installable': True,
     'application': False,
